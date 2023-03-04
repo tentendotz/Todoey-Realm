35ca42ef90351ebdb7d6ece21dd52c944d32ee11
tentendotz <tenten102matsuyama@gmail.com>
2023-03-04 22:08:26 +0900

    Add IB connections to controller

--- Todoey-Realm/TodoListViewController.swift
+++ Todoey-Realm/TodoListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TodoListViewController.swift
 //  Todoey-Realm
 //
 //  Created by tetsuta matsuyama on 2023/03/04.
@@ -7,13 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoListViewController: UITableViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
 
-
+    }
+    
 }
 
