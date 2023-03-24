1b5e2cdbbc52b92bd7a07d6185d043d88d526309
tentendotz <tenten102matsuyama@gmail.com>
2023-03-24 21:52:38 +0900

    Add feature to sort by date-created order

--- Todoey-Realm/Controllers/TodoListViewController.swift
+++ Todoey-Realm/Controllers/TodoListViewController.swift
@@ -84,7 +84,7 @@ class TodoListViewController: UITableViewController {
 extension TodoListViewController: UISearchBarDelegate {
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        todoItems = todoItems?.filter("title CONTAINS[cd] %@", searchBar.text!)
+        todoItems = todoItems?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "dateCreated", ascending: true)
         tableView.reloadData()
     }
     
