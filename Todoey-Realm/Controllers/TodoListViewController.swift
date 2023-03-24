6a00955b6b9b2948a7672c17a82c0d6272a1b683
tentendotz <tenten102matsuyama@gmail.com>
2023-03-25 06:45:05 +0900

    Add feature to pass parent model data via a segue

--- Todoey-Realm/Controllers/TodoListViewController.swift
+++ Todoey-Realm/Controllers/TodoListViewController.swift
@@ -14,6 +14,7 @@ class TodoListViewController: UITableViewController {
     
     let realm = try! Realm()
     var todoItems: Results<Item>?
+    var selectedCategory: Category?
     
     override func viewDidLoad() {
         super.viewDidLoad()
